2d51e609989876c79e3d5677c7bb302dc51b5359
Spencer Alves <impiaaa@gmail.com>
2016-01-29 21:35:25 -0800

    Drawing

--- Assets/Drawer.cs
+++ Assets/Drawer.cs
@@ -1,27 +1,37 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Drawer : MonoBehaviour {
-	public float distanceThreshold = 10.0;
+	public float distanceThreshold = 0.1f;
+    public Transform glowPrefab;
 	Vector2 startingPoint;
 	Vector2 endingPoint;
-	ArrayList<Transform> objects;
+	List<Transform> objects;
 
 	// Use this for initialization
 	void Start () {
-		objects = new ArrayList<Transform> ();
+		objects = new List<Transform> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetMouseButtonDown (0)) {
-			startingPoint = Input.mousePosition;
+        Vector2 point = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        if (Input.GetMouseButtonDown (0)) {
+			startingPoint = point;
 		}
 		if (Input.GetMouseButtonUp (0)) {
-			endingPoint = Input.mousePosition;
+			endingPoint = point;
 		}
 		if (Input.GetMouseButton (0)) {
-			if (Vector2.distance (Input.mousePosition, objects [-1].position) > distanceThreshold) {
+            float distance = 10000;
+            if (objects.Count != 0)
+            {
+                distance = Vector2.Distance(point, objects[objects.Count - 1].position);
+                Debug.Log(distance);
+            }
+            if (objects.Count == 0 || distance > distanceThreshold) {
+                objects.Add((Transform)Instantiate(glowPrefab, point, Quaternion.identity));
 			}
 		}
 	}
